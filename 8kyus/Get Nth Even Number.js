4ddb481cf271a6e29ddfc8d04ac3d36c733d2c25
BirasaDivine <b.divine@alustudent.com>
2024-12-06 11:17:30 +0200

    Nth Even number

---
+++ 8kyus/Get Nth Even Number.js
@@ -0,0 +1,18 @@
+// Return the Nth Even Number
+// Example(Input --> Output)
+// 1 --> 0 (the first even number is 0)
+// 3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+// 100 --> 198
+// 1298734 --> 2597466
+// Initialize an array of whole numbers.
+// Check if the number is even
+// If the number is even check it's index.
+function nthEven(n){
+  let newNumber=n*n;
+  let number;
+  for(let i=0;i<=newNumber;i++){
+      if( number %2 === 0 && indexof){
+          return number.indexOf(n);
+      }
+  }
+}
