b3fd3ff130e00d46ee2d3adfe98e0e6693b30b03
BirasaDivine <b.divine@alustudent.com>
2024-12-07 15:50:25 +0200

    Removed the comments

--- DOMPRACTICE/cloud.js
+++ DOMPRACTICE/cloud.js
@@ -4,16 +4,17 @@ const newText=text.replaceAll(".","").replaceAll(",","").replaceAll(";","").repl
 //console.log(newText);
 const secondText=newText.split(" ");
 //console.log(secondText);
-const thirdText=secondText.filter(x=>x.length >= 1);
-console.log(thirdText);
-let textObject={};
-for(let i=0;i<thirdText.length;i++){
-    let counter = 0;
+const thirdText=secondText.filter(x=>x.length >= 1)
+const forthText=thirdText.map((y)=>y.includes('') ? y.slice(y.length-2));
+console.log(forthText);
+ let textObject={};
+ for(let i=0;i<thirdText.length;i++){
+     let counter = 0;
     for(let y=0;y<thirdText.length;y++){
         if(thirdText[y] == thirdText[i]){
             counter++
         }
     }
     textObject[thirdText[i]] = counter;
-}
+
 console.log(textObject)
