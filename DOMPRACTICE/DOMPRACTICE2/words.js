2dc804b56a9acd9d32b5de5579f0dca1975c6648
BirasaDivine <b.divine@alustudent.com>
2024-12-14 18:49:17 +0200

    Word game

--- DOMPRACTICE/DOMPRACTICE2/words.js
+++ DOMPRACTICE/DOMPRACTICE2/words.js
@@ -16,3 +16,21 @@ document.addEventListener("DOMContentLoaded", function (){
 
     })
 })
+document.addEventListener("DOMContentLoaded", function (){
+    const form=document.querySelector("form");
+    form.addEventListener("submit",function(event){
+        event.preventDefault();
+      
+        const name=document.getElementById('name').value.trim() ;
+        const university=document.getElementById('university').value.trim();
+        const age=parseInt(document.getElementById('age').value,10);
+
+     const personDetails = {
+        name : name;
+        university : university,
+        age : age ;
+     };
+     console.log(personDetails)
+
+    })
+})
