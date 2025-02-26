6dec8a23753f43b7f452567c46c4f0c70c805891
Birasa Divine <b.divine@alustudent.com>
2025-02-26 10:30:12 +0200

    What i like

---
+++ 7kyus/fakeFiles.js
@@ -0,0 +1,9 @@
+// let a = 2;
+// let b = 3;
+// [a, b] = [b, a];
+// console.log(a);
+// console.log(b);
+const colors = ["red", "blue", "green"];
+[colors[0], colors[2]] = [colors[2], colors[0]];
+console.log(colors[0]);
+console.log;
