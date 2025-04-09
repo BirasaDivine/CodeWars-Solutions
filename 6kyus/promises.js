a82d2a9f9dbc3329aebdf524cb6b3cc468c1d57d
Birasa Divine <b.divine@alustudent.com>
2025-04-09 21:14:08 +0200

    Promise

--- 6kyus/promises.js
+++ 6kyus/promises.js
@@ -1,11 +1,71 @@
-const myName = new Promise(function(resolve,reject){
-    let value="Divine"
-    resolve(value);
-})
-const afterMath=function(){
-    myName.then(function(result){
-        console.log(result);
+// function fetchData(){
+//     return    new Promise(function(resolve,reject){
+//       setTimeout(function() {
+//         let success=Math.random()>0.5;
+//         success ? resolve("Data Retrieved") : reject("Failet to retrive")
+//         }, 2000);
+    
+//     })
+// }
+// fetchData()
+// .then( data => console.log(data))
+// .catch( error => console.log(error))
+// .finally( () => console.log("Operation completed"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let promise = new Promise( function(resolve , reject ){
+//     const user = {
+//         name : "Divine",
+//         email : "birasa@gmail.com",
+//         password : "divine32"
+//     }
+//     resolve(user)
+// })
+// promise.then(function (user){
+//     console.log(user)
+// })
+// .catch(function (error){
+//     console.log("No user")
+// })
+// .then(function (){
+//     console.log("Done fetching the user")
+// })
+
+let validateUser = function(userId,password){
+    return new Promise(function(resolve,reject){
+        if(userId && password){
+            resolve("Authenticated")
+        }else{
+            reject({message:"userId and password not authenticated"})
+        }
     })
 }
-afterMath();
- 
+  const app= async (function (){
+        const data = {
+            userId : "",
+            password : "",
+        }
+      try{
+        const result= await validateUser(data)
+         console.log(result)
+      } catch(e){
+        console.log(e.message)
+      }
+        
+  })
+  app();
