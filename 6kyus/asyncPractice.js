2a7954b7acfe53ed8954eec1581d7b06a1b1658e
Birasa Divine <b.divine@alustudent.com>
2025-04-24 18:26:01 +0200

    ASYNC EXERCISES

--- 6kyus/asyncPractice.js
+++ 6kyus/asyncPractice.js
@@ -93,74 +93,110 @@
 // fetch("https://api.example.com/posts")
 // fetch("https://api.example.com/comments")
 // async function fetchUser(){
-//     let url='https://api.example.com/users/1'
-//     fetch(url)
-//     .then(function(response){
+//     let url='https://jsonplaceholder.typicode.com/users/1'
+//     try{
+//         const response= await fetch(url)
 //         if(!response.ok){
 //             throw new Error(`No response ${response.status}`)
 //         }else{
-//             return response.json()
+//             return  response.json()
 //         }
-//     })
-//     .catch(function(error){
-//         return error
-//     })
-// }
-// async function fetchPost(){
-//     let url='https://api.example.com/posts'
-//     fetch(url)
-//     .then(function(response){
-//         if(!response.ok){
-//             throw new Error(`No response ${response.status}`)
-//         }else{
-//             return response.json()
-//         }
-//     })
-//     .catch(function(error){
-//         return error
-//     })
-// }
-// async function fetchComments(){
-//     let url='https://api.example.com/comments'
-//     fetch(url)
-//     .then(function(response){
-//         if(!response.ok){
-//             throw new Error(`No response ${response.status}`)
-//         }else{
-//             return response.json()
-//         }
-//     })
-//     .catch(function(error){
-//         return error
-//     })
-// }
-// async function runTasks(){
-//     let user= await fetchUser()
-//     let posts = await fetchPost(user_id)
-//     let comments= await fetchComments(post_id)
-//     console.log({user,posts,comments})
-// }
-// runTasks()
     
+//     }
+//     catch(error){
+//         return error
+//     }
+// }
+// async function fetchPost(userId){
+//     let url = 'https://jsonplaceholder.typicode.com/posts'
+//     try{
+//         const response= await fetch(url)
+//         if(!response.ok){
+//             throw new Error( `Response error ${response.status} `)
+//         }else{
+//             const posts= await response.json()
+//            return posts.filter(post => post.userId === userId)
+//         }
+//     }
+//     catch(error){
+//         return error
+//     }
+// }
+// async function result(){
+//     const user=await fetchUser();
+//     const post=await fetchPost(user.id)
+//     console.log({user,post})
+// }
+// result()
 // Write an async function that attempts to divide two numbers. 
 // If the denominator is zero, throw an error and handle it gracefully using a try/catch block.
-async function division(num1,num2){
-    try{
+// async function division(num1,num2){
+//     try{
         
-        if(num2 === 0){
-            throw new Error('The denominator is zero')
-        }else{
-            return num1/num2
-        }
-    }
-    catch(error){ 
-        throw error
+//         if(num2 === 0){
+//             throw new Error('The denominator is zero')
+//         }else{
+//             return num1/num2
+//         }
+//     }
+//     catch(error){ 
+//         throw error
+//     }
+// }
+
+// division(12,0).then(function(result){
+//     console.log(result)
+// })
+// .catch(function(error){
+//     console.log(error)
+// })
+
+
+
+
+// Create three async functions that simulate different tasks
+//  (e.g., fetching user data, fetching posts, fetching comments). Ensure they execute in sequence using await.
+
+
+async function fetchUser(){
+    const url='https://jsonplaceholder.typicode.com/users/1'
+    let response = await fetch(url)
+    if( !response.ok){
+        throw new Error(`Response failed ${response.status}`)
+    }else{
+        return await response.json()
     }
 }
-
-division(12,0).then(function(result){
-    console.log(result)
-})
-.catch(function(error){
-    console.log(error)
-})
+async function fetchPost(userId){
+    const url='https://jsonplaceholder.typicode.com/posts'
+    const response= await fetch(url)
+    if(!response.ok){
+        throw new Error(`POST NOT AVAILABLE ${response.status}`)
+    }else{
+        posts= await response.json()
+        return posts.filter(post => post.userId === userId)
+    }
+}
+async function fetchComment(postId){
+    const url='https://jsonplaceholder.typicode.com/comments'
+    const response= await fetch(url)
+    if(!response.ok){
+        throw new Error(`POST NOT AVAILABLE ${response.status}`) 
+    }else{
+        comments= await response.json()
+        return comments.filter(comment => comment.postId === postId)
+    }
+}
+async  function fetchData(){
+    try{
+        let user=await fetchUser()
+    let post=await fetchPost(user.id)
+    let comment= await fetchComment(post[0].id)
+    console.log({user,post,comment})
+    }
+    catch(error){
+        console.log(error)
+    }
+    
+}
+fetchData();
